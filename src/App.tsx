1f41928c2ed0de46347c0da853c4ca63cb9348fa
Lukasso7788 <lukasus7788@gmail.com>
2025-10-05 19:23:35 +0300

    Fix import for RoomPage (default export)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SessionsPage } from './pages/SessionsPage';
-import { RoomPage } from './pages/RoomPage';
+import RoomPage from './pages/RoomPage';
 
 function App() {
   return (
