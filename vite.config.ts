1a0fc1494a16262e092581658f350a6286bd7092
lukasso7788 <lukasus7788@gmail.com>
2025-10-20 19:10:33 +0300

    fix vite base and vercel build routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '', // ✅ именно пустая строка, не './'
   build: {
     outDir: 'dist',
   },
-  base: './', // <-- добавляем это!
-});
+})
