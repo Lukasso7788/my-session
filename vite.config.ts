e9249c05ed162232e112aa912466869ad7201985
lukasso7788 <lukasus7788@gmail.com>
2025-11-06 17:06:49 +0200

    update roompage once again 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // ✅ именно пустая строка, не './'
+  base: '/',       
   build: {
     outDir: 'dist',
   },
