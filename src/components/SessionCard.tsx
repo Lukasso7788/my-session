c07d3f39ab165226e444033415b85bb760a0b375
lukasso7788 <lukasus7788@gmail.com>
2025-11-18 16:35:38 +0200

    session card fix 4

--- src/components/SessionCard.tsx
+++ src/components/SessionCard.tsx
@@ -173,7 +173,7 @@ export default function SessionCard({
       {isHoveringCancel ? (
         // Контент при ховере (Cancel booking)
         <>
-          <img src="/icons/cross-cancel.svg" className="w-4 h-4 mr-2" />{" "}
+          <img src="/icons/cross-cancel.svg" className="w-5 h-5 mr-2" />{" "}
           {/* Предполагается, что 'cross-cancel.svg' существует */}
           Cancel booking
         </>
