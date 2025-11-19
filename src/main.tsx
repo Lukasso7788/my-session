860b82e59b1299071f8fafcfa5606b3ca4d917a1
Lukasso <lukasus7788@gmail.com>
2025-11-19 15:43:17 +0200

    fix3

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 👉 единственная строка, которую нужно добавить
+import "./lib/supabase";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
