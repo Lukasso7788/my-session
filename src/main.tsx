d565179709b0aba1c93c1bfe1d6df301d012e239
Lukasso <lukasus7788@gmail.com>
2025-11-19 18:47:59 +0200

    fix7

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// 👉 единственная строка, которую нужно добавить
+// 👇 вот это критически важно
 import "./lib/supabase";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
