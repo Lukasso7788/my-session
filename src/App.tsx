6ab7a36f2ce8007058c653f6bc56b4c11eda7a99
lukasso7788 <lukasus7788@gmail.com>
2025-11-20 02:26:25 +0200

    fix modal hook5

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,27 @@ import { CreateSessionModalProvider } from "./hooks/useCreateSessionModal";
 import { CreateSessionModal } from "./components/CreateSessionModal";
 
 import SessionsPage from "./pages/SessionsPage";
-import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
+import PublicProfilePage from "./pages/PublicProfilePage";
 
 export default function App() {
   return (
     <CreateSessionModalProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<SessionsPage />} />
           <Route path="/sessions" element={<SessionsPage />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/:id" element={<PublicProfilePage />} />
         </Routes>
 
-        {/* ГЛОБАЛЬНАЯ МОДАЛКА — ВСЕГДА В КОНЦЕ */}
+        {/* глобальная модалка */}
         <CreateSessionModal />
       </BrowserRouter>
     </CreateSessionModalProvider>
