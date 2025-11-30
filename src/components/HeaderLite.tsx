36290f12ec9d9322e7ed154166ad8ebd9475baa0
lukasso7788 <lukasus7788@gmail.com>
2025-11-30 15:16:11 +0200

    fix login and register layout 7

--- src/components/HeaderLite.tsx
+++ src/components/HeaderLite.tsx
@@ -10,7 +10,7 @@ export default function HeaderLite() {
             <div className="w-full max-w-6xl mx-auto px-8 py-6 flex justify-center">
                 <button
                     onClick={() => navigate("/")}
-                    className="text-[36px] font-extrabold leading-none hover:opacity-80 transition"
+                    className="text-[36px] font-extrabold text-brandBlack leading-none hover:opacity-80 transition"
                 >
                     MySession
                 </button>
