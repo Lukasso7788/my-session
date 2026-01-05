4ad910e6e90a90cd1b41e6880ae457428647756f
lukasso7788 <lukasus7788@gmail.com>
2026-01-05 02:21:47 +0200

    iFrame changes 19

--- public/config.js
+++ public/config.js
@@ -1,16 +1,16 @@
 var config = {
     hosts: {
-        domain: "meet.mysession.club",
-        muc: "conference.meet.mysession.club",
+        domain: "meet2.mysession.club",
+        muc: "conference.meet2.mysession.club",
     },
 
     // ✅ Добавил оба варианта, чтобы точно не сломалось
-    bosheURL: "//meet.mysession.club/http-bind", // (оставляем, если где-то у тебя это ожидается)
-    boshURL: "//meet.mysession.club/http-bind",  // (на случай, если ожидается нормальное имя)
-    websocket: "wss://meet.mysession.club/xmpp-websocket",
+    bosheURL: "//meet2.mysession.club/http-bind", // (оставляем, если где-то у тебя это ожидается)
+    boshURL: "//meet2.mysession.club/http-bind",  // (на случай, если ожидается нормальное имя)
+    websocket: "wss://meet2.mysession.club/xmpp-websocket",
 
     // optional: backup domain (если где-то хочешь юзать)
-    backupDomain: "meet2.mysession.club",
+    backupDomain: "meet.mysession.club",
 
     defaultLanguage: "en",
     enableWelcomePage: true,
