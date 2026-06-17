9b6856c7df5db32697e0c5602b4cf40e8ee36b4f
lukasso7788 <lukasus7788@gmail.com>
2026-06-17 10:44:02 +0300

    Fix broken supabase import

--- src/components/IntentionsPanel.tsx
+++ src/components/IntentionsPanel.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../lib/supabase";
 
 type AiHostStage = {
   name?: string;
